0ac91ef5c5cfe0dafe7e34c4e32d7baf0cab9b16
sanjay <sanjay@pgagi.in>
2024-05-26 15:27:44 +0000

    deploy script added

---
+++ node_deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+NAME="pgagi-website"
+
+pm2 del $NAME
+
+rm -rf .next
+npm install
+npm run build
+
+pm2 start npm --name $NAME --watch --env production -- start -- -p 5000
+pm2 save 
+pm2 startup
