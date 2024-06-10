97f97141da21369f44fccde4d1d5d2024ba0e0fc
Pratik <pratik@pgagi.in>
2024-06-11 00:09:44 +0530

    correction

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,47 +1,3 @@
-// import Image from "next/image";
-// import Link from "next/link";
-// import { links, resources, socialList, services } from "@/utils/constants";
-// import styles from "./footer.module.scss";
-// import logo from '../assets/logo.png';
-
-// export default function Footer() {
-//     return (
-//         <section className={styles.footer}>
-//             <div className={styles.wrapper}>
-//             <div className={styles.about}>
-//                 <div className={styles.services}>
-//                     <h6>Services</h6>
-//                     {services.map((service, i) => (<span key={i}>{service}</span>))}
-//                 </div>
-//                     <div className={styles.list}>
-//                         <h6>Resources</h6>
-//                         {links.map((link, i) => (<span key={i}>{link}</span>))}
-//                     </div>
-//                     <div className={styles.company}>
-//                         <h6>Company</h6>
-//                         <span>
-//                             <a href="https://pgagi.in/aboutus">About Us</a>
-//                             <a href="https://x.com/PGAGI123?t=hAoqjn4ffAoYXjIp9yt-ug&s=09">Twitter</a>
-//                             <a href="https://www.instagram.com/pgagi_pvt.ltd/">Instagram</a>
-//                             <a href="https://www.linkedin.com/company/pg-agi/">LinkedIn</a>
-//                             <a href="https://youtube.com/@pg-agi?feature=shared">Youtube</a>
-//                         </span>
-//                     </div>
-//                     <div className={styles.signUp}>
-//                         <h6>Sign up for our newsletter to stay up to date</h6>
-//                         <span className={styles.input}>
-//                             <input type="text" title="Your Email Id......"></input>
-//                         </span>
-//                     </div>
-//                 </div>
-                              
-//             </div>
-//             {/* <div className={styles.copyright}>
-//                 Copyright©2024PG-AGI
-//             </div> */}
-//         </section>
-//     );
-// }
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
@@ -50,10 +6,10 @@ import styles from "./footer.module.scss";
 import logo from '../assets/logo.png';
 
 export default function Footer() {
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
       const response = await fetch("http://localhost:5000/newsletter", {
@@ -119,17 +75,15 @@ export default function Footer() {
         </div>
       </div>
       <div className={styles.footerText}>
-                <p className={styles.pgHidden}>Playing God With</p>
-                <p className={styles.agiHidden}>AGI</p>
-
-                <p className={styles.pg}>Playing God</p>
-                <p className={styles.agi}>With AGI</p>
-            
+        <p className={styles.pgHidden}>Playing God With</p>
+        <p className={styles.agiHidden}>AGI</p>
+        <p className={styles.pg}>Playing God</p>
+        <p className={styles.agi}>With AGI</p>
       </div>
-      <div className={styles.copyright}> 
-      <div className={styles.flexing}>
-        <p>Copyright©2024</p>
-        <p className={styles.right}>Privacy Policy</p>
+      <div className={styles.copyright}>
+        <div className={styles.flexing}>
+          <p>Copyright©2024</p>
+          <p className={styles.right}>Privacy Policy</p>
         </div>      
       </div>
     </section>
