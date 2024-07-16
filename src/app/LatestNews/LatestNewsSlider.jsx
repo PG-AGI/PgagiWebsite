a27b67aaedfc76aab030d1760acd1e4bdde5b79d
unknown <shreyans@pgagi.in>
2024-07-16 20:54:17 +0530

    Updated Components

--- src/app/LatestNews/LatestNewsSlider.jsx
+++ src/app/LatestNews/LatestNewsSlider.jsx
@@ -1,27 +1,25 @@
+"use-client"
 import React from 'react';
 import Link from 'next/link';
-
+import Image from 'next/image';
+import styles from './LatestNewsSlider.module.scss';
 const LatestNewsSlider = ({ latestPosts }) => {
   return (
-    <div className="mb-8">
-      <div className="flex space-x-4 ">
+    <div className={styles.main}>
+      <div className={styles.sect}>
         {latestPosts.map((post) => (
           <Link href={`/posts/${post.slug}`} key={post.slug}
-          className="relative w-full h-60 bg-gray-900  rounded-3xl transition-transform transform "
-          >    
-            
-              <img
-                src={post.thumbnail}
-                alt={post.title}
-                className="object-cover w-full h-60 rounded-3xl mb-2  opacity-30"
-              />
-              <div className="absolute bottom-0 left-0 right-0 text-white p-4">
-                <h3 className="text-lg font-semibold mb-2">{post.title}</h3>
-                <p className="text-gray-300 text-sm">
+            className={styles.poster}
+          >
+            <div className={styles.poster} style={{ backgroundImage: `url(${post.thumbnail})` }}>
+              <div className={styles.posterblur}></div>
+              <div className={styles.postertitle}>
+                <p className={styles.postername}>{post.title}</p>
+                <p className={styles.posterdetails}>
                   By {post.author} • {post.publishDate}
                 </p>
               </div>
-            
+            </div>
           </Link>
         ))}
       </div>
--- src/app/components/LatestTrends.jsx
+++ src/app/components/LatestTrends.jsx
@@ -1,14 +1,15 @@
+'use-client'
 import React from 'react';
 import LatestNews from '../LatestNews/LatestNews';
-
+import styles from './LatestTrends.module.scss';
 export default function LatestTrends() {
   return (
-    <div className="w-full m-0 xl:w-4/5">
-      <h6 className="text-lg font-semibold mb-4 ml-4">Latest Trends & News</h6>
+    <div className={styles.main}>
+      <h6>Latest Trends & News</h6>
       <div>
         {LatestNews.map((latest, index) => (
-          <div key={index} className="mb-3 items-center hover:bg-gray-100 m-3 p-4"
-          style={{borderRadius: '30px'}} >
+          <div key={index} className={styles.newsitems}
+            style={{ borderRadius: '30px' }} >
             <a
               href={latest.link}
               target="_blank"
