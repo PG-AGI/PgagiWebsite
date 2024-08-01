cdb25362348e7e60dcebb02db23569760448a496
SahilSinha3 <sahil@pgagi.in>
2024-08-01 14:32:32 +0530

    nav bar issues

--- src/app/components/base/Navigation.tsx
+++ src/app/components/base/Navigation.tsx
@@ -1,4 +1,143 @@
-'use client'
+// 'use client'
+
+// import Image from "next/image";
+// import logo from '../../assets/logo.png';
+// import styles from "./navigation.module.scss";
+// import Link from "next/link";
+// import { useEffect, useState } from "react";
+// import clsx from "clsx";
+// import { whatWeDoLinks } from "@/utils/constants";
+// import BookCallModal from "./bookCallModela";
+// import ContactUsForm from "./contactUsForm";
+
+// export default function Navigation() {
+//   const [lastScrollTop, setLastScrollTop] = useState(0);
+//   const [navbarVisible, setNavbarVisible] = useState(true);
+//   const [whatWeDo, setWhatWeDo] = useState<'solutions' | 'industries' | 'caseStudy'>('solutions');
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const [isMenuOpen, setIsMenuOpen] = useState(false);
+//   const [isMobile, setIsMobile] = useState(false);
+
+//   const [isSolutionOpen, setIsSolutionOpen] = useState(false);
+//   const [isIndustriesOpen, setIndustriesOpen] = useState(false);
+//   const [isCaseStudyOpen, setIsCaseStudyOpen] = useState(false);
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setIsMobile(window.innerWidth <= 768); 
+//     };
+
+//     handleResize(); // set initial value
+//     window.addEventListener('resize', handleResize);
+
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     };
+//   }, []);
+
+//   const handleContactUs = () => {
+//     setIsModalOpen(true);
+//   };
+
+//   const handleCloseModal = () => {
+//     setIsModalOpen(false);
+//   };
+
+//   const toggleMenu = () => {
+//     setIsMenuOpen(!isMenuOpen);
+//   };
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       let currentScrollPos = window.pageYOffset;
+//       if (currentScrollPos > lastScrollTop) {
+//         // Scrolling down
+//         setNavbarVisible(false);
+//       } else {
+//         // Scrolling up
+//         setNavbarVisible(true);
+//       }
+//       setLastScrollTop(currentScrollPos); // Update lastScrollTop
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+//     return () => window.removeEventListener("scroll", handleScroll);
+//   }, [lastScrollTop]);
+
+//   useEffect(() => {
+//     if (isMenuOpen) {
+//       document.body.style.overflow = 'hidden';
+//     } else {
+//       document.body.style.overflow = 'auto';
+//     }
+//   }, [isMenuOpen]);
+
+//   return (
+//     <nav className={styles.navigation}>
+//       <div className={styles.banner}>
+//         AI Calling Agent -{" "}
+//         <a href="https://www.toingg.com/" target="_blank" rel="noopener noreferrer">
+//           Try for Free
+//         </a>
+//       </div>
+//       <Link className={clsx(styles.logo, !navbarVisible && styles.logoHidden)} href='/'>
+//         <Image src={logo} alt='Logo' width={60} height={60} />
+//         <p>PG-AGI</p>
+//       </Link>
+//       <div
+//         className={clsx(styles.nav, !navbarVisible && styles.navHidden, isMenuOpen && styles.open)}
+//       >       
+//         <div className={styles.links}>
+//           <div className={styles.whatWeDo}>
+//             <a>What we do</a>
+//             <div className={styles.dropdown}>
+//               <div className={styles.content}>
+//                 <span className={styles.background} />
+//                 <div className={styles.menu}>
+//                   <span className={clsx(whatWeDo === 'solutions' && styles.active)} onMouseEnter={() => setWhatWeDo('solutions')}>Solutions</span>
+//                   {isMobile && isSolutionOpen && (
+//                     <div className={styles.dropdownLinks}>
+//                       {whatWeDoLinks.solutions.map((link, index) => <a key={index}>{link}</a>)}
+//                     </div>
+//                   )}
+//                   <span className={clsx(whatWeDo === 'industries' && styles.active)} onMouseEnter={() => setWhatWeDo('industries')}>Industries</span>
+//                   {isMobile && isIndustriesOpen && (
+//                     <div className={styles.dropdownLinks}>
+//                       {whatWeDoLinks.industries.map((link, index) => <a key={index}>{link}</a>)}
+//                     </div>
+//                   )}
+//                   <span className={clsx(whatWeDo === 'caseStudy' && styles.active)} onMouseEnter={() => setWhatWeDo('caseStudy')}>Case Study</span>
+//                   {isMobile && isCaseStudyOpen && (
+//                     <div className={styles.dropdownLinks}>
+//                       {whatWeDoLinks.caseStudy.map((link, index) => <a key={index}>{link}</a>)}
+//                     </div>
+//                   )}
+//                 </div>
+//                 {!isMobile && (
+//                   <div className={styles.dropdownLinks}>
+//                     {whatWeDoLinks[whatWeDo].map((link, index) => <a key={index}>{link}</a>)}
+//                   </div>
+//                 )}
+//               </div>
+//             </div>
+//           </div>
+//           <Link href="/blogs">What we think</Link>
+//           <Link href="/aboutUs">Who We Are</Link>
+//         </div>
+//         <button className={styles.contact} onClick={handleContactUs}>Contact us</button>
+//         <div className={`${styles.hamburger} ${isMenuOpen ? styles.open : ''}`} onClick={toggleMenu}>
+//           <span></span>
+//           <span></span>
+//           <span></span>
+//         </div>        
+//       </div>
+//       {isModalOpen && <ContactUsForm onClose={handleCloseModal}/>}
+//     </nav>
+//   );
+// }
+
+// throtling 
+// 'use client'
 
 import Image from "next/image";
 import logo from '../../assets/logo.png';
@@ -10,6 +149,17 @@ import { whatWeDoLinks } from "@/utils/constants";
 import BookCallModal from "./bookCallModela";
 import ContactUsForm from "./contactUsForm";
 
+// Throttle function to limit the number of times a function is called
+function throttle<T extends (...args: any[]) => void>(fn: T, wait: number) {
+  let time = Date.now();
+  return function (...args: Parameters<T>) {
+    if (time + wait - Date.now() < 0) {
+      fn(...args);
+      time = Date.now();
+    }
+  };
+}
+
 export default function Navigation() {
   const [lastScrollTop, setLastScrollTop] = useState(0);
   const [navbarVisible, setNavbarVisible] = useState(true);
@@ -18,8 +168,9 @@ export default function Navigation() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
+  // Define state for dropdown visibility
   const [isSolutionOpen, setIsSolutionOpen] = useState(false);
-  const [isIndustriesOpen, setIndustriesOpen] = useState(false);
+  const [isIndustriesOpen, setIsIndustriesOpen] = useState(false);
   const [isCaseStudyOpen, setIsCaseStudyOpen] = useState(false);
 
   useEffect(() => {
@@ -47,8 +198,25 @@ export default function Navigation() {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  // Toggle dropdowns for mobile view
+  const toggleDropdown = (section: 'solutions' | 'industries' | 'caseStudy') => {
+    if (section === 'solutions') {
+      setIsSolutionOpen(!isSolutionOpen);
+      setIsIndustriesOpen(false);
+      setIsCaseStudyOpen(false);
+    } else if (section === 'industries') {
+      setIsIndustriesOpen(!isIndustriesOpen);
+      setIsSolutionOpen(false);
+      setIsCaseStudyOpen(false);
+    } else if (section === 'caseStudy') {
+      setIsCaseStudyOpen(!isCaseStudyOpen);
+      setIsSolutionOpen(false);
+      setIsIndustriesOpen(false);
+    }
+  };
+
   useEffect(() => {
-    const handleScroll = () => {
+    const handleScroll = throttle(() => {
       let currentScrollPos = window.pageYOffset;
       if (currentScrollPos > lastScrollTop) {
         // Scrolling down
@@ -58,7 +226,7 @@ export default function Navigation() {
         setNavbarVisible(true);
       }
       setLastScrollTop(currentScrollPos); // Update lastScrollTop
-    };
+    }, 100); // Throttle to limit execution frequency
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
@@ -94,19 +262,37 @@ export default function Navigation() {
               <div className={styles.content}>
                 <span className={styles.background} />
                 <div className={styles.menu}>
-                  <span className={clsx(whatWeDo === 'solutions' && styles.active)} onMouseEnter={() => setWhatWeDo('solutions')}>Solutions</span>
+                  <span 
+                    className={clsx(whatWeDo === 'solutions' && styles.active)}
+                    onMouseEnter={() => setWhatWeDo('solutions')}
+                    onClick={() => toggleDropdown('solutions')}
+                  >
+                    Solutions
+                  </span>
                   {isMobile && isSolutionOpen && (
                     <div className={styles.dropdownLinks}>
                       {whatWeDoLinks.solutions.map((link, index) => <a key={index}>{link}</a>)}
                     </div>
                   )}
-                  <span className={clsx(whatWeDo === 'industries' && styles.active)} onMouseEnter={() => setWhatWeDo('industries')}>Industries</span>
+                  <span 
+                    className={clsx(whatWeDo === 'industries' && styles.active)}
+                    onMouseEnter={() => setWhatWeDo('industries')}
+                    onClick={() => toggleDropdown('industries')}
+                  >
+                    Industries
+                  </span>
                   {isMobile && isIndustriesOpen && (
                     <div className={styles.dropdownLinks}>
                       {whatWeDoLinks.industries.map((link, index) => <a key={index}>{link}</a>)}
                     </div>
                   )}
-                  <span className={clsx(whatWeDo === 'caseStudy' && styles.active)} onMouseEnter={() => setWhatWeDo('caseStudy')}>Case Study</span>
+                  <span 
+                    className={clsx(whatWeDo === 'caseStudy' && styles.active)}
+                    onMouseEnter={() => setWhatWeDo('caseStudy')}
+                    onClick={() => toggleDropdown('caseStudy')}
+                  >
+                    Case Study
+                  </span>
                   {isMobile && isCaseStudyOpen && (
                     <div className={styles.dropdownLinks}>
                       {whatWeDoLinks.caseStudy.map((link, index) => <a key={index}>{link}</a>)}
@@ -131,7 +317,7 @@ export default function Navigation() {
           <span></span>
         </div>        
       </div>
-      {isModalOpen && <ContactUsForm onClose={handleCloseModal}/>}
+      {isModalOpen && <ContactUsForm onClose={handleCloseModal} />}
     </nav>
   );
 }
