87725393900632a720b584e5ff15a487f64db721
Harsimran-19 <harsimran1869@gmail.com>
2024-10-05 15:33:50 +0530

    connected events pages

--- src/app/components/Webinars.tsx
+++ src/app/components/Webinars.tsx
@@ -1,35 +1,33 @@
+import Link from 'next/link';
 import { webinarList } from "@/utils/constants";
 import styles from './webinar.module.scss';
 
 export default function Webinars() {
-  
-    return (
-      <section className={styles.webinar}>
-        <div className={styles.webinarHead}>
+  return (
+    <section className={styles.webinar}>
+      <div className={styles.webinarHead}>
         <h3>{"Webinars and Events"}</h3>
-        <button>View More</button>
-        </div>
-        
-        <div className={styles.webinarList}>
-          {webinarList.map((item, i) => (
-            <div
-              key={i}
-              className={styles.webinarItem}
-            >
-              <div className={styles.content}>
-                <h4>{item.type}</h4>
-                <h3>{item.title}</h3>
-                <p>{item.description}</p>
-                <a href={item.link}>
-                <button>{item.status?'Enroll Now':'Coming Soon'}</button>
-                </a>
-              </div>
-              <div className={styles.circle}></div>
+        <a href={'/events'}><button>View More</button></a>
+      </div>
+      
+      <div className={styles.webinarList}>
+        {webinarList.map((item) => (
+          <div
+            key={item.id}
+            className={styles.webinarItem}
+          >
+            <div className={styles.content}>
+              <h4>{item.type}</h4>
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+              <Link href={item.link}>
+                <button>{item.status ? 'Enroll Now' : 'Coming Soon'}</button>
+              </Link>
             </div>
-          ))}
-  
-        </div>
-      </section>
-    );
-  }
-  
+            <div className={styles.circle}></div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
