d82b3e829af064ec40cbe23c69758b786cee2b91
Harsimran-19 <harsimran1869@gmail.com>
2024-10-17 11:33:26 +0530

    added productDetails

--- src/app/components/products.tsx
+++ src/app/components/products.tsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import styles from './products.module.scss';
 import { productData } from '@/utils/constants';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Products: React.FC = () => {
   const [hoverIndex, setHoverIndex] = useState<number | null>(null);
 
   return (
-    <section className={styles.products}>
+    <section className={styles.main} id="products">
+      <h1>Our Products</h1>
+      <div className={styles.products}>
       {productData.map((product, index) => (
         <div
           key={index}
@@ -16,14 +19,22 @@ const Products: React.FC = () => {
           onMouseLeave={() => setHoverIndex(null)}
         >
            <div className={styles.icon}>
-            <Image src={product.icon} alt={product.title} width={100} height={100} /> {/* Use Next.js Image */}
-            {hoverIndex === index ? <h3>{product.miniTitle}</h3> : <h2>{product.title}</h2>}
+            <Image src={product.icon} alt={product.title} width={150} height={150} /> {/* Use Next.js Image */}
+            {hoverIndex === index ? <h3>{product.miniTitle}</h3> : (
+              <div>
+                <h2>{product.title}</h2>
+                <h4>{product.subtitle}</h4>
+              </div>
+              )}
           </div>
           <p className={hoverIndex === index ? styles.show : styles.hide}>{product.description}</p>
+          <a className={styles.productLink} href={product.link}>
           <button className={styles.viewButton}>View &raquo;</button>
+          </a>
           <div className={styles.circle}></div>
         </div>
       ))}
+      </div>
     </section>
   );
 };
