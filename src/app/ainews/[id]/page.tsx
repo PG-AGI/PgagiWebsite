a63faf75ef27d913489c529b1885ac518af058a3
tauheed06 <tauheed@pgagi.in>
2025-01-21 17:30:37 +0530

    adding metatags for case study, blogs and ainews, dynamically/api, optional

--- src/app/ainews/[id]/page.tsx
+++ src/app/ainews/[id]/page.tsx
@@ -7,21 +7,22 @@ export default function BlogPostPage() {
   )
 }
 
-// app/[dynamicPage]/page.js
+// Note: Please don't remove the file structure for BlogPost, CaseStudy or Ainews
+// because this generateMetadata() method requires a Server Side Component.
 export async function generateMetadata({ params }: { params: { id: string } }) {
   const id = params.id as string;
   try {
-    // const metaData = await getMetaTags({ contentType: 'blogs', pageId: id });
+    const metaData = await getMetaTags({ contentType: 'blogs', pageId: id });
 
-    // const metaDescription = metaData.description;
-    // const metaKeywords = metaData.keywords;
-    // const metaAuthor = metaData.author;
-    // const appTitle = metaData.title;
+    const metaDescription = metaData.description;
+    const metaKeywords = metaData.keywords;
+    const metaAuthor = metaData.author;
+    const appTitle = metaData.title;
 
-      const metaDescription = "AI-powered business solutions for your company";
-      const metaKeywords = "AI, business, solutions, technology, innovation";
-      const metaAuthor = "AI-powered business solutions for your company";
-      const appTitle = "AI-powered business solutions for your company";
+      // const metaDescription = "AI-powered business solutions for your company";
+      // const metaKeywords = "AI, business, solutions, technology, innovation";
+      // const metaAuthor = "AI-powered business solutions for your company";
+      // const appTitle = "AI-powered business solutions for your company";
     return {
       title: appTitle,
       description: metaDescription,
