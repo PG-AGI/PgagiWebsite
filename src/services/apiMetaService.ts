ccaf257641dc054524997073de86635c6c135c21
swappgagi <swapnendu@pgagi.in>
2025-01-23 11:31:14 +0530

    Update BASE URL

--- src/services/apiMetaService.ts
+++ src/services/apiMetaService.ts
@@ -5,7 +5,7 @@ interface contentType{
     pageId: string,
 }
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://pgagi.in/api',
     timeout: 10000, 
     headers: {
       'Content-Type': 'application/json', 
