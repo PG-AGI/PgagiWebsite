a72673ebde09e53dad60e5b8713722aea10e159d
tauheed06 <tauheed@pgagi.in>
2025-01-30 17:55:27 +0530

    build issue resolved

--- src/app/ainews/[slug]/Ainews.tsx
+++ src/app/ainews/[slug]/Ainews.tsx
@@ -494,10 +494,12 @@ const Ainews = () => {
               {
                 news.map(data => {
                   return (
-                    <div className={styles.card}>
-                      <Link href={`/ainews/${data.slug}`} key={data.slug} legacyBehavior>
+                    <div className={styles.card} key={data.slug}>
+                      <Link href={`/ainews/${data.slug}`}  legacyBehavior>
                         <div className={styles.cardImageContainer}>
-                          <img
+                          <Image
+                            height={500}
+                            width={1000}
                             src={data.coverImage}
                             alt="Card"
                             className={styles.cardImage}
