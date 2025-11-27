5d0b83c596a009dcdf10097086415f0dd3f18bf4
Himanshu-1613 <himanshu@pgagi.in>
2025-11-27 17:43:36 +0530

    our partner updated

--- src/app/components/Partners.tsx
+++ src/app/components/Partners.tsx
@@ -5,7 +5,7 @@ import ciek from '../assets/partners/ciek2.png'
 import aixl from '../assets/partners/aixl2.png'
 import Marquee from "react-fast-marquee";
 import o from '../assets/partners/o.png'
-import logok from '../assets/partners/logok.png'
+import logok from '../assets/partners/logo1.png'
 import wcg from '../assets/partners/wcg-logo.png'
 import fomo from '../assets/partners/fomo.png'
 import tvc from '../assets/partners/TVClogo (1).png'
@@ -16,24 +16,26 @@ import Image from "next/image";
 
 import styles from "./partners.module.scss";
 
-const images = [ais, hx, social, ciek, aixl,o,logok,wcg,fomo,tvc,Ms];
+const images = [ais, hx, social, ciek, aixl, o, logok, wcg, fomo, tvc, Ms];
 
-export default function Partners () {
+export default function Partners() {
     return (
         <section id="partners" className={styles.partners}>
             <h3>Our Trusted Partners</h3>
-            
-            <Marquee className={styles.marquee} speed={55} >
-            <div className={styles.partnerList}>
-                {
-                    images.map((image, i) => (
-                        <Image key={i} src={image} alt='Sponsored Partner' />
-                    ))
-                }
-                            </div>
-                    </Marquee>
 
-        
+            <Marquee className={styles.marquee} speed={55} >
+                <div className={styles.partnerList}>
+                    {
+                        images.map((image, i) => (
+                            <div key={i} className={styles.card}>
+                                <Image src={image} alt='Sponsored Partner' />
+                            </div>
+                        ))
+                    }
+                </div>
+            </Marquee>
+
+
         </section>
     )
 }
