1149a55e9cf4ef6c90e90b7036f3505c9eedd934
KulwantSinghh <kulwant@pgagi.in>
2026-03-17 12:37:41 +0530

    WhatareweBuildng section

--- src/app/components/NewPage.tsx
+++ src/app/components/NewPage.tsx
@@ -40,60 +40,103 @@ const NewPage = () => {
       <div className={styles.container}>
         {/* Header Section */}
         <div className={styles.header}>
-          <div className={styles.titleWrapper}>
+          <div className={styles.titleContainer}>
             <h1 className={styles.title}>
-              What are we <span className={styles.accentText}>building?</span>
+              What are we <span className={styles.highlight}>building?</span>
             </h1>
-            <div className={styles.titleAvatars}>
-              <div className={styles.avatarS}>S</div>
-              <div className={styles.avatarA}>A</div>
-            </div>
           </div>
         </div>
 
         {/* Dynamic Connecting Lines (Desktop Only) */}
         {!isMobile && (
           <div className={styles.linesContainer}>
-            <svg className={styles.svgLines} viewBox="0 0 1000 280" preserveAspectRatio="none">
-              <defs>
-                <filter id="shadow" x="-20%" y="-20%" width="140%" height="140%">
-                  <feGaussianBlur in="SourceAlpha" stdDeviation="5" />
-                  <feOffset dx="0" dy="10" result="offsetblur" />
-                  <feComponentTransfer>
-                    <feFuncA type="linear" slope="0.3" />
-                  </feComponentTransfer>
-                  <feMerge>
-                    <feMergeNode />
-                    <feMergeNode in="SourceGraphic" />
-                  </feMerge>
-                </filter>
-              </defs>
-
-              <g filter="url(#shadow)">
-                {/* 1. BLACK PIPE BASE (Thickest) */}
-                {/* Trunk */}
-                <path d="M500,0 L500,80" className={styles.lineLayerBlack} fill="none" />
-                {/* Left Branch */}
-                <path d="M500,80 Q500,120 460,120 L250,120 Q210,120 210,160 L210,280" className={styles.lineLayerBlack} fill="none" />
-                {/* Right Branch */}
-                <path d="M500,80 Q500,120 540,120 L750,120 Q790,120 790,160 L790,280" className={styles.lineLayerBlack} fill="none" />
-
-                {/* 2. RED LINE (Right/Top) */}
-                {/* Trunk */}
-                <path d="M504,0 L504,80" className={styles.lineLayerRed} fill="none" />
-                {/* Left Branch Turn (Red is Inner) */}
-                <path d="M504,80 Q504,116 464,116 L250,116 Q214,116 214,156 L214,280" className={styles.lineLayerRed} fill="none" />
-                {/* Right Branch Turn (Red is Outer) */}
-                <path d="M504,80 Q504,124 544,124 L750,124 Q794,124 794,164 L794,280" className={styles.lineLayerRed} fill="none" />
-
-                {/* 3. BLUE LINE (Left/Bottom) */}
-                {/* Trunk */}
-                <path d="M496,0 L496,80" className={styles.lineLayerBlue} fill="none" />
-                {/* Left Branch Turn (Blue is Outer) */}
-                <path d="M496,80 Q496,124 456,124 L250,124 Q206,124 206,164 L206,280" className={styles.lineLayerBlue} fill="none" />
-                {/* Right Branch Turn (Blue is Inner) */}
-                <path d="M496,80 Q496,116 536,116 L750,116 Q786,116 786,156 L786,280" className={styles.lineLayerBlue} fill="none" />
+            <svg width="100%" height="100%" viewBox="0 0 562 220" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.svgLines} preserveAspectRatio="none">
+              <g filter="url(#filter0_577_7628)">
+                <path d="M277.425 2.11426V76.6756L278.619 90.8739C279.496 101.299 288.213 109.314 298.675 109.314H517.119C528.235 109.314 537.246 118.325 537.246 129.441V213.905" stroke="#4B6F95" strokeWidth="10.9784" />
               </g>
+              <g filter="url(#filter1_577_7628)">
+                <path d="M277.884 0.742188V70.2718V79.5815C277.884 92.2131 288.123 102.453 300.755 102.453H519.865C532.496 102.453 542.736 112.693 542.736 125.325V207.044" stroke="#2C2C2C" strokeWidth="10.9784" />
+              </g>
+              <g filter="url(#filter2_577_7628)">
+                <path d="M277.426 -11.1514V64.8846V73.1771C277.426 85.3035 287.256 95.1338 299.382 95.1338H528.098C540.224 95.1338 550.055 104.964 550.055 117.091V199.725" stroke="#9F0000" strokeWidth="10.9784" />
+              </g>
+              <g filter="url(#filter3_577_7628)">
+                <path d="M280.427 -33.5654V40.9959L279.233 55.1942C278.356 65.6194 269.638 73.6346 259.176 73.6346H40.7324C29.6166 73.6346 20.6055 82.6458 20.6055 93.7616V186.459" stroke="#4B6F95" strokeWidth="10.9784" />
+              </g>
+              <g filter="url(#filter4_577_7628)">
+                <path d="M279.968 -34.9375V34.5921V44.8167C279.968 56.943 270.138 66.7734 258.011 66.7734H37.0719C24.9456 66.7734 15.1152 76.6037 15.1152 88.7301V188.746" stroke="#9F0000" strokeWidth="10.9784" />
+              </g>
+              <g filter="url(#filter5_577_7628)">
+                <path d="M280.426 -46.8311V29.2049V32.9231C280.426 47.5758 268.548 59.4541 253.895 59.4541H34.3279C19.6752 59.4541 7.79688 71.3325 7.79688 85.9852V195.608" stroke="#2C2C2C" strokeWidth="10.9784" />
+              </g>
+              <defs>
+                <filter id="filter0_577_7628" x="268.276" y="0.284532" width="278.118" height="219.11" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                  <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                  <feOffset dy="1.82973" />
+                  <feGaussianBlur stdDeviation="1.82973" />
+                  <feComposite in2="hardAlpha" operator="out" />
+                  <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                  <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_577_7628" />
+                  <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_577_7628" result="shape" />
+                </filter>
+                <filter id="filter1_577_7628" x="268.735" y="-1.08754" width="283.149" height="213.621" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                  <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                  <feOffset dy="1.82973" />
+                  <feGaussianBlur stdDeviation="1.82973" />
+                  <feComposite in2="hardAlpha" operator="out" />
+                  <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                  <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_577_7628" />
+                  <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_577_7628" result="shape" />
+                </filter>
+                <filter id="filter2_577_7628" x="268.276" y="-12.9811" width="290.926" height="218.195" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                  <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                  <feOffset dy="1.82973" />
+                  <feGaussianBlur stdDeviation="1.82973" />
+                  <feComposite in2="hardAlpha" operator="out" />
+                  <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                  <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_577_7628" />
+                  <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_577_7628" result="shape" />
+                </filter>
+                <filter id="filter3_577_7628" x="11.4577" y="-34.4803" width="278.118" height="227.343" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                  <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                  <feOffset dy="2.74459" />
+                  <feGaussianBlur stdDeviation="1.82973" />
+                  <feComposite in2="hardAlpha" operator="out" />
+                  <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.18 0" />
+                  <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_577_7628" />
+                  <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_577_7628" result="shape" />
+                </filter>
+                <filter id="filter4_577_7628" x="5.9675" y="-35.8524" width="283.149" height="231.002" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                  <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                  <feOffset dy="2.74459" />
+                  <feGaussianBlur stdDeviation="1.82973" />
+                  <feComposite in2="hardAlpha" operator="out" />
+                  <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.18 0" />
+                  <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_577_7628" />
+                  <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_577_7628" result="shape" />
+                </filter>
+                <filter id="filter5_577_7628" x="-1.35086" y="-47.7459" width="290.926" height="249.757" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                  <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                  <feOffset dy="2.74459" />
+                  <feGaussianBlur stdDeviation="1.82973" />
+                  <feComposite in2="hardAlpha" operator="out" />
+                  <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.18 0" />
+                  <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_577_7628" />
+                  <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_577_7628" result="shape" />
+                </filter>
+                <clipPath id="clip0_577_7628">
+                  <rect width="562" height="171" fill="white" />
+                </clipPath>
+                <clipPath id="clip1_577_7628">
+                  <rect width="289" height="163" fill="white" transform="translate(270 51)" />
+                </clipPath>
+              </defs>
             </svg>
           </div>
         )}
@@ -101,7 +144,7 @@ const NewPage = () => {
         {/* Content Grid */}
         <div className={styles.cardsGrid}>
           {/* Left Card: Foundations */}
-          <div className={styles.card}>
+          <div className={`${styles.card} ${styles.cardFounders}`}>
             <div className={styles.cardHeaderWrapper}>
               <div className={styles.cardVisualLeft}>
                 <div className={styles.cardGhostTitle}>Build a Custom AI Product —</div>
@@ -115,11 +158,13 @@ const NewPage = () => {
                     <span>Build, test, and refine...</span>
                   </div>
                 </div>
-                <div className={styles.formulaBox}>
+                <div className={styles.formulaArea}>
                   <p className={styles.formulaLabel}>AI-generated formula</p>
-                  <code className={styles.formulaText}>
-                    <span className={styles.highlightCyan}>{"{{"}AI Idea{"}}"}</span>&<span className={styles.highlightOrange}>{"{{"}MVP{"}}"}</span>?
-                  </code>
+                  <div className={styles.formulaBox}>
+                    <code className={styles.formulaText}>
+                      <span className={styles.highlightCyan}>{"{{"}AI Idea{"}}"}</span>&<span className={styles.highlightOrange}>{"{{"}MVP{"}}"}</span>?
+                    </code>
+                  </div>
                 </div>
               </div>
               <div className={styles.badgesContainer}>
@@ -147,7 +192,7 @@ const NewPage = () => {
           </div>
 
           {/* Right Card: Enterprises */}
-          <div className={styles.card}>
+          <div className={`${styles.card} ${styles.cardEnterprises}`}>
             <div className={`${styles.cardHeaderWrapper} ${styles.rightHeaderGradient}`}>
               <div className={styles.cardVisualRight}>
                 <div className={styles.cardGhostTitleWhite}>Implement AI Into Your Business</div>
@@ -161,11 +206,13 @@ const NewPage = () => {
                     <span>Integrate AI tools with...</span>
                   </div>
                 </div>
-                <div className={styles.formulaBox}>
+                <div className={styles.formulaArea}>
                   <p className={styles.formulaLabel}>AI-generated formula</p>
-                  <code className={styles.formulaText}>
-                    <span className={styles.highlightOrange}>{"{{"}Processes{"}}"}</span>&<span className={styles.highlightCyan}>{"{{"}AI Tools{"}}"}</span>?
-                  </code>
+                  <div className={styles.formulaBox}>
+                    <code className={styles.formulaText}>
+                      <span className={styles.highlightOrange}>{"{{"}Processes{"}}"}</span>&<span className={styles.highlightCyan}>{"{{"}AI Tools{"}}"}</span>?
+                    </code>
+                  </div>
                 </div>
               </div>
               <div className={styles.badgesContainer}>
@@ -182,10 +229,7 @@ const NewPage = () => {
                 Automate workflows, reduce costs, and improve decision-making by integrating AI into your existing systems — without disrupting operations.
               </p>
               <div className={styles.ctaWrapper}>
-                <div className={styles.ctaAvatars}>
-                  <div className={styles.avatarS}>S</div>
-                  <div className={styles.avatarA}>A</div>
-                </div>
+
                 <button className={styles.pillButton}>
                   Let's build together
                   <div className={styles.redArrowCircle}>
