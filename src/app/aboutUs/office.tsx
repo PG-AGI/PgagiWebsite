3078605ec245f7f68bff64531b1e70755d298fbf
GajapathiRaoDev <venkata@pgagi.in>
2026-07-31 05:16:20 +0530

    Add OfficeSection gallery with responsive image grid layout

--- src/app/aboutUs/office.tsx
+++ src/app/aboutUs/office.tsx
@@ -13,6 +13,23 @@ const OfficeSection: React.FC = () => {
           Together, we create, collaborate, and bring bold ideas to life.
         </p>
       </div>
+      <div className={styles.gallery}>
+        {[
+          "/assets/office/office1.jpg",
+          "/assets/office/office2.jpg",
+          "/assets/office/office3.jpg",
+          "/assets/office/office4.jpg",
+          "/assets/office/office5.jpg",
+          "/assets/office/office6.jpg",
+        ].map((src, i) => (
+          <div
+            key={src}
+            className={`${styles.item} ${i === 5 ? styles.large : ""}`}
+          >
+            <img src={src} alt={`Office ${i + 1}`} loading="lazy" />
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
